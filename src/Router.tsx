1cbfc32b1eeaf1e7426233daad6ad8245f3d08cd
이찬호 <chanholee275@gmail.com>
2022-07-17 12:40:47 +0900

    layout: eslint와 prettier 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './component/Header';
 import List from './component/List';
 
-const Router = () => <BrowserRouter><Header/><List></List></BrowserRouter>; 
+const Router = () => (
+  <BrowserRouter>
+    <Header />
+    <List></List>
+  </BrowserRouter>
+);
 
 export default Router;
