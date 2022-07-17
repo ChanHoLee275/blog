0365dcc05a3039d12cea0bedd9119de5d0849fbe
이찬호 <chanholee275@gmail.com>
2022-07-17 21:07:15 +0900

    feat: 페이지로 분리

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './component/Header';
-import List from './component/List';
+import Main from './page/Main';
 
 const Router = () => (
   <BrowserRouter>
     <Header />
-    <List>
-      <a href="https://www.flaticon.com/free-icons/growth" title="growth icons">
-        Growth icons created by Freepik - Flaticon
-      </a>
-    </List>
+    <Route path="/" element={<Main />} />
   </BrowserRouter>
 );
 
---
+++ src/page/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from '../../component/List';
+
+const Main = () => (
+  <List>
+    <a href="https://www.flaticon.com/free-icons/growth" title="growth icons">
+      Growth icons created by Freepik - Flaticon
+    </a>
+  </List>
+);
+
+export default Main;
