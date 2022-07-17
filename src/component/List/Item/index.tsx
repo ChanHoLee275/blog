25a4b2b11365f33f0583ffbfc80b5b6408729d21
이찬호 <chanholee275@gmail.com>
2022-07-17 23:46:22 +0900

    feat: post link 추가

--- src/component/List/Item/index.tsx
+++ src/component/List/Item/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -7,11 +9,16 @@ import { CardActionArea } from '@mui/material';
 export interface ItemProps {
   title: string;
   abstract: string;
+  link?: string;
 }
 
-const Item: React.FC<ItemProps> = ({ title, abstract }) => {
+const Item: React.FC<ItemProps> = ({ title, abstract, link }) => {
+  const navigate = useNavigate();
   return (
-    <Card sx={{ minWidth: 345, width: '60%', backgroundColor: '#f2f2f7', margin: '8px' }}>
+    <Card
+      sx={{ minWidth: 345, width: '60%', backgroundColor: '#f2f2f7', margin: '8px' }}
+      onClick={() => navigate(link ?? '/blog/post')}
+    >
       <CardActionArea>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
