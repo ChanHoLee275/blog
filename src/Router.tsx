f9df41f8e5a66f104ef88b9b265deb230fa3e88d
이찬호 <chanholee275@gmail.com>
2022-07-20 22:51:13 +0900

    feat: 전체 화면에 메뉴 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Category, { CategoryProps, PostsProps } from './component/Category';
 import Header from './component/Header';
 import Menu from './component/Menu';
 import Main from './page/Main';
 import Post from './page/Post';
+
+const postsDummy: PostsProps[] = [{ title: 'title1', url: '' }];
 
 const Router = () => {
   const [open, setOpen] = React.useState(false);
@@ -11,7 +14,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Header setOpen={setOpen} />
-      <Menu open={open} setOpen={setOpen} />
+      <Menu open={open} setOpen={setOpen} children={<Category title="test" posts={postsDummy} />} />
       <Routes>
         <Route path="/blog" element={<Main />} />
         <Route path="/blog/post" element={<Post />} />
